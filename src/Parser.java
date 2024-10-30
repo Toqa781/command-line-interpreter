562d644fd563f5ba8e0538e1fc1297fd07deedb2
Kernashaat203 <147157199+Kernashaat203@users.noreply.github.com>
2024-10-30 21:59:10 +0300

    Update Parser.java

--- src/Parser.java
+++ src/Parser.java
@@ -1,9 +1,12 @@
 public class Parser {
     private String cmd;
     private String[] args;
+    private boolean showAll = false;
+    private boolean reverseOrder = false;
     public Parser(String[] command){
         cmd=command[0];
         args=command;
+        parseFlags();
     }
     public String getCmd(){
         return cmd;
@@ -18,5 +21,25 @@ public class Parser {
         if(args.length<3)
             return "";
         else return args[2];
+    }
+    private void parseFlags() {
+        for (int i = 1; i < args.length; i++) {
+            switch (args[i]) {
+                case "-a":
+                    showAll = true;
+                    break;
+                case "-r":
+                    reverseOrder = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public boolean isShowAll() {
+        return showAll;
+    }
+    public boolean isReverseOrder() {
+        return reverseOrder;
     }
 }
